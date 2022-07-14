afa7c49ea6b733982aa673f190b6248e5477367c
FusionSid <siddheshadsv@icloud.com>
2022-07-14 18:39:25 +1200

    Got working dark mode toggle working and also decided not to use daisy ui.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
     darkMode: "class",
     content: ["./src/*.{html,js}"],
     theme: {
-        extend: {},
-    },
-    plugins: [require("daisyui")],
+        extend: {
+            fontFamily: {
+                taro: ["Taro"],
+                aileron: ["Aileron"]
+            },
+            backgroundImage: {
+                light_mode_bg: "url('../images/light_gradient.jpeg')",
+                dark_mode_bg: "url('../images/dark_gradient.jpeg')"
+            }
+        },
+    }
 };
