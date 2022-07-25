a13b1463938a5d2684e5f88ecef216df545e0213
FusionSid <siddheshadsv@icloud.com>
2022-07-25 20:45:20 +1200

    updated index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const electron = require("electron");
-const { app, BrowserWindow, Menu, ipcMain } = electron;
+const { app, BrowserWindow, Menu, ipcMain, ipcRenderer } = electron;
+const { WebSocket } = require("ws");
 
 // Local imports
 const { getAccessToken } = require("./scripts/requests");
@@ -23,7 +24,7 @@ Menu.setApplicationMenu(mainMenu);
 global.app = app;
 
 /* Window Creation */
-let mainWindow, signUpWindow;
+let mainWindow, roomWindow;
 // Creates main window, Launched on startup
 function createMainWindow() {
     // Create window
@@ -43,8 +44,8 @@ function createMainWindow() {
 }
 
 //
-function createSignUpWindow() {
-    signUpWindow = new BrowserWindow({
+function createRoomWindow() {
+    roomWindow = new BrowserWindow({
         webPreferences: {
             contextIsolation: false,
             nodeIntegration: true,
@@ -52,7 +53,7 @@ function createSignUpWindow() {
     });
 
     // load in index.html file
-    signUpWindow.loadFile(path.join(__dirname, "html/join.html"));
+    roomWindow.loadFile(path.join(__dirname, "html/room.html"));
 }
 
 /* Electron App Events: */
@@ -79,7 +80,28 @@ app.on("activate", () => {
 /* IPC Main Events */
 
 // When someone hits login button with info
-ipcMain.on("login-and-join-room", function (event, data) {
-    let token = await getAccessToken(data.username, data.password)
-    
+ipcMain.on("login-and-join-room", async function (event, data) {
+    createRoomWindow();
+    mainWindow.close();
+
+    const access_token = await getAccessToken(data.username, data.password);
+    const room_id = data.room_id;
+
+    var ws = new WebSocket(
+        `ws://0.0.0.0:443/api/ws/chatroom?access_token=${access_token}&room_id=${room_id}`
+    );
+
+    ws.onmessage = function (event) {
+        let msg = event.data;
+        ipcRenderer.send("new-message-room-ws", msg);
+    };
+
+    ipcMain.on("send-message-ws", function (event, data) {
+        ws.send(
+            JSON.stringify({
+                message_content: data,
+                access_token: access_token,
+            })
+        );
+    });
 });
